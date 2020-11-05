ebd43edd99bc2da72ae535f4e8585f91780a0ef0
maaverik <nithinjosetom@gmail.com>
2020-11-06 01:52:22 +0530

    Extending index

--- index.ts
+++ index.ts
@@ -1,9 +1,24 @@
-import opine from "https://deno.land/x/opine@0.25.0/mod.ts";
+import { opine, serveStatic } from 'https://deno.land/x/opine@0.12.0/mod.ts';
+import { renderFileToString } from 'https://deno.land/x/dejs@0.7.0/mod.ts';
+import { join, dirname } from 'https://deno.land/x/opine@main/deps.ts';
 
-const app = opine();
+import { ensureAuthenticated } from './middleware/authmiddleware.ts';
+import users from './controllers/usercontroller.ts';
+import auth from './controllers/authcontroller.ts';
+
+const app = opine();    // web server
+const dirname_ = dirname(import.meta.url);
+
+app.engine(".html", renderFileToString);    // serve static html
+app.use(serveStatic(join(dirname_, "public")));
+app.set("view engine", "html");
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.render("index", {title: "Hello world"});
 });
 
-app.listen(5000);
+app.use("/users", ensureAuthenticated, users);  // authenticator middleware
+app.use("/auth", auth);
+
+app.listen(3000);
+console.log("Running app on localhost:3000......")
