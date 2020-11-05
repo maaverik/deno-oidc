25a658548bfc27296d1cc740d720fbd3fbcdd314
maaverik <nithinjosetom@gmail.com>
2020-11-06 01:56:56 +0530

    Added user route

---
+++ src/controllers/usercontroller.ts
@@ -0,0 +1,9 @@
+import { Router } from "https://deno.land/x/opine@0.12.0/mod.ts";
+
+const userRoutes = new Router();
+
+userRoutes.get("/me", (req, res) => {   // render HTML in the /users/me file
+    res.render("/users/me", { title: "Profile", user: res.app.locals.user });
+});
+
+export default userRoutes;
